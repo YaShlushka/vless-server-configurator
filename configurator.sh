b621a63a41cc9d8db754e2ed4bd45e00961bf235
YaShlushka <paderinee@gmail.com>
2026-06-01 19:47:57 +0300

    bla

--- configurator.sh
+++ configurator.sh
@@ -3,8 +3,7 @@ echo -e "$PASSWORD\n$PASSWORD" | passwd
 apt update && apt upgrade -y
 useradd -m -s /bin/bash "$USERNAME"
 echo "$USERNAME:$PASSWORD" | chpasswd
-sed -i '/#Port 22/c\Port $SSH_PORT' /etc/ssh/sshd_config
-sed -i '/PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
+sed -i "/#Port 22/c\Port $SSH_PORT" /etc/ssh/sshd_config
+sed -i "/PermitRootLogin/c\PermitRootLogin no" /etc/ssh/sshd_config
 systemctl restart sshd
 apt install git nmap net-tools curl docker.io docker-compose openssl -y
-
