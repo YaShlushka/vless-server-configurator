a5d6cdc250efb5458081f06c45cf87e1f10b626b
YaShlushka <paderinee@gmail.com>
2026-06-01 19:59:25 +0300

    bla

--- configurator.sh
+++ configurator.sh
@@ -7,3 +7,5 @@ sed -i "/#Port 22/c\Port $SSH_PORT" /etc/ssh/sshd_config
 sed -i "/PermitRootLogin/c\PermitRootLogin no" /etc/ssh/sshd_config
 systemctl restart sshd
 apt install git nmap net-tools curl docker.io docker-compose openssl -y
+curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh -o $HOME/vless-server-configurator/install.sh
+bash $HOME/vless-server-configurator/install.sh v2.9.4 --ci --port "$PANEL_PORT" --username "$USERNAME" --password "$PASSWORD"
