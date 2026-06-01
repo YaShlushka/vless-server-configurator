8a901ef6af6ddcff647a41a7cb46527ec42b8cb5
YaShlushka <paderinee@gmail.com>
2026-06-01 19:40:20 +0300

    bla

--- configurator.sh
+++ configurator.sh
@@ -7,4 +7,4 @@ sed -i '/#Port 22/c\Port $SSH_PORT' /etc/ssh/sshd_config
 sed -i '/PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
 systemctl restart sshd
 apt install git nmap net-tools curl docker.io docker-compose openssl -y
-git clone https://github.com/MHSanaei/3x-ui.git
+
