66245904cd047f32e4a2f1cecfd892bc70e41bc1
YaShlushka <paderinee@gmail.com>
2026-06-01 23:05:18 +0300

    bla

--- warp.sh
+++ warp.sh
@@ -3,7 +3,9 @@ curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --ou
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt update
 sudo apt install cloudflare-warp
-warp-cli registration new
+warp-cli registration new << EOF
+y
+EOF
 warp-cli mode proxy
 warp-cli proxy port $WARP_PORT
 warp-cli connect
